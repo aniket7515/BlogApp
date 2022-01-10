887cce4355d57cd2cb4dfc44324585c895d8e6e3
Aniket <aniketdhokane2071@gmail.com>
2022-01-10 18:52:29 +0530

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 function Home() {
+    const handleClick=() => {
+        console.log("Hello Ninja");
+    }
+    const handleClickAgain=(name) => {
+        console.log("hello"+ name);
+    }
+
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={()=>{
+                handleClickAgain("Aniket")
+            }}>Click me Again</button>
         </div>
     )
 }
