49a9e3486c6611a1510dc17d984db067edc1959a
Aniket <aniketdhokane2071@gmail.com>
2022-01-10 18:40:36 +0530

    Add Styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,15 @@ function Navbar() {
     return (
         <div>
             <nav className="navbar">
-                <h1>The Dojo Blog</h1>
+                <h1>The React Blog</h1>
                 <div className="links">
                     <a href="/">Home</a>
-                    <a href="/create">New Blog</a>
-                    
+                    <a href="/create" style={{
+                        color:"white",
+                        backgroundColor:"#f1356d",
+                        borderRadius:"8px"
+                    }}>New Blog</a>
+
                 </div>
             </nav>
         </div>
