126408e5a8c335d205e8c2e05698f49625c6fd6f
Aniket <aniketdhokane2071@gmail.com>
2022-01-10 19:01:05 +0530

    using State (useState Hook)

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Home() {
+    // let name="Mario"
+    const[name,setName]=useState('mario');
+    const[age,setAge]=useState(25)
+
+
+
     const handleClick=() => {
-        console.log("Hello Ninja");
+        setName("Aniket")
+        setAge(20)
     }
-    const handleClickAgain=(name) => {
-        console.log("hello"+ name);
-    }
+    
 
 
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <p>{name} is {age}</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={()=>{
-                handleClickAgain("Aniket")
-            }}>Click me Again</button>
+            
         </div>
     )
 }
